24b425b4d249a0c7d77d906be7daa77d825381e8
Adarsht-07 <157559564+Adarsht-07@users.noreply.github.com>
2024-01-25 20:32:43 +0530

    Add files via upload

--- Assignment2_Q.2_23CE02026.c
+++ Assignment2_Q.2_23CE02026.c
@@ -2,11 +2,11 @@
 
 int main(){
     int a,b,c,max;
-    printf("Enter the first numbers:");
+    printf("Enter the first number:");
     scanf("%d",&a);
-    printf("Enter the second numbers:");
+    printf("Enter the second number:");
     scanf("%d",&b);
-    printf("Enter the third numbers:");
+    printf("Enter the third number:");
     scanf("%d",&c);
     max=a>b?a:b;
     max=max>c?max:c;
